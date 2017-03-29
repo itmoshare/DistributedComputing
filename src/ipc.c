53638b707619164bbc3cf58b2a883839d64fb7dd
DMokhnatkin <dmitriy.mokhnatkin@gmail.com>
2017-03-29 20:10:46 +0300

    Args input

---
+++ src/ipc.c
@@ -0,0 +1,18 @@
+#include "ipc.h"
+
+int send(void * self, local_id dst, const Message * msg) {
+	
+}
+
+int send_multicast(void * self, const Message * msg) {
+	
+}
+
+int receive(void * self, local_id from, Message * msg) {
+	
+}
+
+int receive_any(void * self, Message * msg) {
+	
+}
+
--- src/main.c
+++ src/main.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
+#include <getopt.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[])
+int main(int argc, char * const argv[])
 {
-	printf("aaa\n");
+	if (argc < 2) {
+		fprintf(stderr, "Not enough arguments\n");
+        return 1;
+    }
+	
+	int n = 0;
+	switch (getopt(argc, argv, "p:")) {
+		case 'p':
+			n = atoi(optarg);
+			if (!n || n <= 0 || n > 10) {
+				fprintf(stderr, "p parameter must be int from 1 to 10\n");
+				return 1;
+			}
+			break;
+		case -1:
+			return 1;
+	}
+	
+	
+	
 	return 0;
 }
